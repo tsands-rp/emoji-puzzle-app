d669bc1d00d0d7b7aff2d988b890db65a137a64a
Trey Sands <tsands@rightpoint.com>
2019-06-14 09:49:49 -0500

    Updates Emojigram VC to adhere to better style
    Removes crash from accessing an empty list.
    Organizes methods into semantically related groups.
    Renamed methods to follow same convention.
    Refactors code so functions do one thing.
    Adds a title screen.
    Moves current Emojigram from VC to store.
    Adds a give up button after user has tried a hint and receiving part of the answer

---
+++ Emojigrams/Emojigrams/EmojigramTitleScreenViewController.swift
@@ -0,0 +1,15 @@
+//
+//  EmojigramTitleScreenViewController.swift
+//  Emojigrams
+//
+//  Created by Trey Sands on 6/14/19.
+//  Copyright © 2019 Trey Sands. All rights reserved.
+//
+
+import UIKit
+
+class EmojigramTitleScreenViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
